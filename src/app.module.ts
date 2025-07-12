924681891431a01ad8e3cd60a383150cedb67583
Fernando Bonifacio <fernando.bonifacio@bepass.com.br>
2025-07-12 18:47:47 -0300

    chore(setup): go go :), initialize project with NestJS, Docker and PostgreSQL integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5434', 10),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_DATABASE || 'finance_db',
+      autoLoadEntities: true,
+      //synchronize: false, sai truta
+    }),
+  ],
 })
 export class AppModule {}
